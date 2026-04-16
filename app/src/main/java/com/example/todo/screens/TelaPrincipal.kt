d7ab9af6ed7237b68489ec4684f218180a796195
Arthur de Sá Tenório <arthursa147@gmail.com>
2026-04-16 16:06:43 -0300

    feat: addialog de confirmaçao ao deletar tarefas

--- app/src/main/java/com/example/todo/screens/TelaPrincipal.kt
+++ app/src/main/java/com/example/todo/screens/TelaPrincipal.kt
@@ -1,6 +1,7 @@
 package com.example.todo.screens
 
 import android.annotation.SuppressLint
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -14,10 +15,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.Divider
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -25,10 +26,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextDecoration
@@ -78,6 +82,30 @@ fun ListaTarefas(navegacao: NavController) {
 
     val tarefas = viewModel.tarefas.collectAsState().value
 
+    // Confirmação de exclusão genérica
+    var abrirDialog by remember { mutableStateOf(false) }
+    var tarefaSelec by remember { mutableStateOf<Tarefa?>(null) }
+
+    if (abrirDialog && tarefaSelec != null) {
+        AlertDialog(
+            title = { Text("Confirmação de Exclusão") },
+            text = { Text("Deseja excluir a tarefa: \"${tarefaSelec!!.conteudo}\"?") },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        viewModel.deletar(tarefaSelec!!);
+                        abrirDialog = false
+                        Toast.makeText(contexto, "Tarefa deletada!", Toast.LENGTH_SHORT).show()
+                    }
+                ) { Text("Sim") }
+            },
+            dismissButton = {
+                Button(onClick = { abrirDialog = false }) { Text("Não") }
+            },
+            onDismissRequest = { abrirDialog = false }
+        )
+    }
+
     LazyColumn(
         modifier = Modifier
             .padding(horizontal = 15.dp)
@@ -126,7 +154,10 @@ fun ListaTarefas(navegacao: NavController) {
                     Spacer(Modifier.width(10.dp))
 
                     Button(
-                        onClick = { viewModel.deletar(tarefa) },
+                        onClick = {
+                            tarefaSelec = tarefa;
+                            abrirDialog = true;
+                        },
                         shape = RoundedCornerShape(corner = CornerSize(5.dp)),
                         contentPadding = PaddingValues(horizontal = 2.dp, vertical = 1.dp),
                         colors = ButtonColors(
